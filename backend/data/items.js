47a80c37cd59d9df1999f56dbff79483aee326a0
Vivek Singh Negi <viveksinghnegi1605@gmail.com>
2026-07-18 10:30:19 +0530

    update path in backend

--- backend/data/items.js
+++ backend/data/items.js
@@ -1,14 +1,17 @@
-const fs = require('node:fs/promises');
+const fs = require("node:fs/promises");
+const path = require("path");
+
+const filePath = path.join(__dirname, "items.json");
 
 async function getStoredItems() {
-  const rawFileContent = await fs.readFile('items.json', { encoding: 'utf-8' });
+  const rawFileContent = await fs.readFile(filePath, { encoding: "utf-8" });
   const data = JSON.parse(rawFileContent);
   const storedItems = data.items ?? [];
   return storedItems;
 }
 
 function storeItems(items) {
-  return fs.writeFile('items.json', JSON.stringify({ items: items || [] }));
+  return fs.writeFile(filePath, JSON.stringify({ items: items || [] }));
 }
 
 exports.getStoredItems = getStoredItems;
